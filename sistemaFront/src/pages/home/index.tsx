6a63564180c3a92dde19aa88d705af34e464b8b2
Anna Victoria Feliciana <anna.feliciana@upe.br>
2025-05-11 13:40:02 -0300

    Redirect para a página inicial

--- sistemaFront/src/pages/home/index.tsx
+++ sistemaFront/src/pages/home/index.tsx
@@ -1,19 +1,23 @@
 import * as S from './style'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
+
+  const handleLogin = () => {
+    // aqui pode adicionar lógica de validação futuramente
+    navigate('/principal')
+  }
+
   return (
     <S.Container>
       <S.Title>Agendamento de visitas</S.Title>
       <S.Card>
-        <S.LoginBoxText>
-          Usuário:
-        </S.LoginBoxText>
+        <S.LoginBoxText>Usuário:</S.LoginBoxText>
         <S.Input placeholder="Insira o seu usuário" />
-        <S.LoginBoxText>
-          Senha:
-        </S.LoginBoxText>
+        <S.LoginBoxText>Senha:</S.LoginBoxText>
         <S.Input type="password" placeholder="Insira a sua senha" />
-        <S.Button>Entrar</S.Button>
+        <S.Button onClick={handleLogin}>Entrar</S.Button>
       </S.Card>
     </S.Container>
   )
--- sistemaFront/src/routes/index.tsx
+++ sistemaFront/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/home'
+import Principal from '../pages/principal'
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/principal" element={<Principal />} />
         {/* Outras rotas virão aqui futuramente */}
       </Routes>
     </BrowserRouter>
