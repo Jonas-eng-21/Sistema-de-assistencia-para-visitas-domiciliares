9d77a9e8597aa366f58ce471e04bc18a42ee6736
Anna Victoria Feliciana <anna.feliciana@upe.br>
2025-05-11 13:17:08 -0300

    Estilização página de login

--- sistemaFront/src/pages/home/index.tsx
+++ sistemaFront/src/pages/home/index.tsx
@@ -1,26 +1,20 @@
-import reactLogo from '../../assets/react.svg'
-import viteLogo from '/vite.svg'
-import CountButton from '../../components/CountButton'
 import * as S from './style'
 
 const Home = () => {
   return (
     <S.Container>
-      <S.LogoGroup>
-        <a href="https://vite.dev" target="_blank">
-          <S.Logo src={viteLogo} alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <S.Logo src={reactLogo} className="react" alt="React logo" />
-        </a>
-      </S.LogoGroup>
-      <S.Title>Vite + React</S.Title>
+      <S.Title>Agendamento de visitas</S.Title>
       <S.Card>
-        <CountButton />
+        <S.LoginBoxText>
+          Usuário:
+        </S.LoginBoxText>
+        <S.Input placeholder="Insira o seu usuário" />
+        <S.LoginBoxText>
+          Senha:
+        </S.LoginBoxText>
+        <S.Input type="password" placeholder="Insira a sua senha" />
+        <S.Button>Entrar</S.Button>
       </S.Card>
-      <S.Paragraph>
-        Click on the Vite and React logos to learn more
-      </S.Paragraph>
     </S.Container>
   )
 }
--- sistemaFront/src/pages/home/style.ts
+++ sistemaFront/src/pages/home/style.ts
@@ -6,39 +6,56 @@ export const Container = styled.div`
   align-items: center;
 `
 
-export const LogoGroup = styled.div`
-  display: flex;
-  gap: 2rem;
-  margin-bottom: 1rem;
-`
-
-export const Logo = styled.img`
-  height: 6rem;
-  will-change: filter;
-  transition: filter 300ms;
-  &:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-
-  &.react:hover {
-    filter: drop-shadow(0 0 2em #61dafbaa);
-  }
-`
-
 export const Title = styled.h1`
-  font-size: 3rem;
+  font-size: 2rem;
   margin-bottom: 1rem;
 `
 
 export const Card = styled.div`
+  width: 100%;
+  max-width: 400px;
   padding: 2em;
-  background-color: #f4f4f5;
-  border-radius: 12px;
+  background-color: #ffffff;
+  border-radius: 10px;
   margin-bottom: 1rem;
   box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
+  
 `
 
-export const Paragraph = styled.p`
-  font-size: 0.875rem;
-  color: #888;
+export const LoginBoxText = styled.p`
+  font-size: 1rem;
+  margin-bottom: 0.2rem;
+  font-weight: 500;
+  color: black;
+  text-align: left;
+`
+
+// Botão entrar
+export const Button = styled.button`
+  background-color: #98B8F3;
+  color: black;
+  margin-top: 1.5rem;
+  padding: 0.5em 5em;
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+  font-size: 1.3rem;
+  width: 100%;
+
+  &:hover {
+    background-color:#81a8f0;
+  }
+`
+
+// Input de texto do login
+export const Input = styled.input`
+  padding: 0.6em;
+  border-radius: 10px;
+  border: 1px solid #ccc;
+  margin-top: 0.2rem;
+  margin-bottom: 1rem;
+  margin-right: 0.5rem;
+  font-size: 1rem;
+  width: 95%;
+  background-color: #F6F9FF;
 `
