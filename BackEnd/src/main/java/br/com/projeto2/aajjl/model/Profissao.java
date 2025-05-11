c24e290dc05265fb3b116b2a79a0d646932364af
Antonio Sereno <antoniojsereno@gmail.com>
2025-05-11 18:58:23 -0300

    Adicionando o Backend no projeto
    O backend foi desenvolvido no projeto no github de Joao Pedro e agr esta sendo levado para o projeto Frontend para podermos realizar a integração dos dois e fazer o projeto funcionar.
    
    https://github.com/PedroFerreiraLopes/Projeto-2-_-Back-End

---
+++ BackEnd/src/main/java/br/com/projeto2/aajjl/model/Profissao.java
@@ -0,0 +1,19 @@
+package br.com.projeto2.aajjl.model;
+
+public enum Profissao {
+    MEDICO,
+    ENFERMEIRO,
+    ENFERMEIRO_CHEFE,
+    ASSISTENTE_SOCIAL,
+    PACIENTE,
+    AGENTE_DE_SAÚDE_ACS,
+    CLINICO_GERAL,
+    NUTRICIONISTA,
+    FISIOTERAPEUTA,
+    DENTISTA,
+    PSICOLOGO,
+    PSIQUIATRA,
+    PEDIATRA
+
+}
+
