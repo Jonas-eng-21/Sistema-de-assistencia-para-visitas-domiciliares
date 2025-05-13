355a61eb22a9a823d16081b4fbf5eac67bc90a01
Anna Victoria Feliciana <anna.feliciana@upe.br>
2025-05-13 19:58:07 -0300

    Esqueleto do header

---
+++ sistemaFront/src/components/Header/style.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  display: flex;
+    justify-content: space-between;
+    width: 100%;
+    background-color: #BDD0F5;
+`
+
+export const HeaderAtalhos = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-left: 2em;
+    align-items: left;
+    gap: 2em;
+    margin-right: 2em;
+`
+
+export const HeaderLadoDireito = styled.div`
+  display: flex;
+    justify-content: space-between;
+    margin-right: 2em;
+    align-items: right;
+    gap: 1em;
+`
--- sistemaFront/src/pages/principal/index.tsx
+++ sistemaFront/src/pages/principal/index.tsx
@@ -1,8 +1,16 @@
 import * as S from './style'
+import Header from '../../components/Header'
+import React from 'react';
 
 const Principal = () => {
+  // Set the page title
+  React.useEffect(() => {
+    document.title = "Página Inicial - Agendamento de visitas";
+  }, []);
+
   return (
     <S.Container>
+      <Header />
       <S.Title>PAGINA INICIAL</S.Title>
     </S.Container>
   )
