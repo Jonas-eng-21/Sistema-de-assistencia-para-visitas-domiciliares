9b0f595df6ce7097d3795954aa87e4cd63c4f5db
Anna Victoria Feliciana <anna.feliciana@upe.br>
2025-05-14 23:55:15 -0300

    página inicial

--- sistemaFront/src/pages/principal/index.tsx
+++ sistemaFront/src/pages/principal/index.tsx
@@ -11,7 +11,28 @@ const Principal = () => {
   return (
     <S.Container>
       <Header />
-      <S.Title>PAGINA INICIAL</S.Title>
+      <div style={{ display: "flex", width: "100%" }}>
+        <S.LadoEsquerdo>
+          <S.Subtitle>Agendamento de Visitas</S.Subtitle>
+          <S.ProximasVisitas>
+            <p> Listagem das proximas visitas </p>
+          </S.ProximasVisitas>
+        </S.LadoEsquerdo>
+        <S.LadoDireito>
+          <S.Button2>
+            <img src="/calendar-plus.svg" alt="Agendar visita" style={{ width: 75, height: 85 }} />
+            <S.TextoButton2>Agendar <br /> Visita</S.TextoButton2>
+          </S.Button2>
+          <S.Button2>
+            <img src="/calendar.svg" alt="Calendário de visitas" style={{ width: 75, height: 85 }} />
+            <S.TextoButton2>Calendário <br /> de visitas</S.TextoButton2>
+          </S.Button2>
+          <S.Button2>
+            <img src="/list.svg" alt="Listar Pacientes" style={{ width: 75, height: 85 }} />
+            <S.TextoButton2>Listar <br /> Pacientes</S.TextoButton2>
+          </S.Button2>
+        </S.LadoDireito>
+      </div>
     </S.Container>
   )
 }
