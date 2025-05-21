552c2e560c8637220af8533b55eac295b9d942e4
Luís Eduardo <luiseduardo0501@protonmail.com>
2025-05-21 17:28:44 -0300

    Update ResponseDTO.java

--- BackEnd/src/main/java/br/com/projeto2/aajjl/dto/ResponseDTO.java
+++ BackEnd/src/main/java/br/com/projeto2/aajjl/dto/ResponseDTO.java
@@ -1,13 +1,13 @@
 package br.com.projeto2.aajjl.dto;
 
-import br.com.projeto2.aajjl.model.Profissao;
+import br.com.projeto2.aajjl.model.Profession;
 
 public record ResponseDTO(
         String nome,
         String cpf,
         String consenhoRegional,
         String email,
-        br.com.projeto2.aajjl.model.Profession profissao,
+        Profession profissao,
         Boolean ativo,
         String token
 ) {}
