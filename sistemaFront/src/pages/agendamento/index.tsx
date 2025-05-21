dff6b72dc0483161ed73e16d28ffd7cbb064dabd
Anna Victoria Feliciana <anna.feliciana@upe.br>
2025-05-21 19:37:04 -0300

    estrutura básica

--- sistemaFront/src/pages/agendamento/index.tsx
+++ sistemaFront/src/pages/agendamento/index.tsx
@@ -3,53 +3,74 @@ import Header from '../../components/Header'
 import React from 'react';
 
 const Principal = () => {
-  // Set the page title
-  React.useEffect(() => {
-    document.title = "Página Inicial - Agendamento de visitas";
-  }, []);
+    // Set the page title
+    React.useEffect(() => {
+        document.title = "Página Inicial - Agendamento de visitas";
+    }, []);
 
-  return (
-    <S.Container>
-      <Header />
-    <form>
-        <S.FormGroup>
-            <label htmlFor="paciente">Paciente:</label>
-            <select id="paciente" name="paciente">
-                <option value="paciente1">Paciente 1</option>
-                <option value="paciente2">Paciente 2</option>
-                <option value="paciente3">Paciente 3</option>
-            </select>
-        </S.FormGroup>
-        <S.FormGroup>
-            <label htmlFor="paciente">Profissional:</label>
-            <select id="profissional" name="profissional">
-                <option value="profissional1">Profissional 1</option>
-                <option value="profissional2">Profissional 2</option>
-                <option value="profissional3">Profissional 3</option>
-            </select>
-        </S.FormGroup>
-        <S.FormGroup>
-            <label htmlFor="data">Data da Visita:</label>
-            <input type="date" id="data" name="data" />
-        </S.FormGroup>
-        <S.FormGroup>
-            <label htmlFor="Turno">Turno:</label>
-            <select id="turno" name="turno">
-                <option value="manha">Manhã</option>
-                <option value="tarde">Tarde</option>
-            </select>
-        </S.FormGroup>
-        <S.FormGroup>
-            <label htmlFor="observacoes">Observações:</label>
-            <textarea id="observacoes" name="observacoes" rows={4} />
-        </S.FormGroup>
-        <S.FormGroup>
-            <button type="submit">Agendar</button>
-        </S.FormGroup>
+    return (
+        <S.Container>
+            <Header />
+            <div style={{ display: 'flex', width: '100%', height: '100px', flexDirection: 'column', alignItems: 'center' }}>
+                <S.AgendamentoTitle>Agendamento de Visitas</S.AgendamentoTitle>
+                <form style={{ width: '90%', height: '100%' }}>
+                    <S.FormGroup>
+                        <S.FormRow>
+                            <S.FormGroup>
+                                <S.FormFonte htmlFor="paciente">Paciente:</S.FormFonte>
+                                <S.SelectForm id="paciente" name="paciente">
+                                    <option value="paciente1">Paciente 1</option>
+                                    <option value="paciente2">Paciente 2</option>
+                                    <option value="paciente3">Paciente 3</option>
+                                </S.SelectForm>                                
+                            </S.FormGroup>
+                            <S.FormGroup>
+                                <S.TextoAjuda>Caso não encontre, <br/> cadastre um novo paciente:</S.TextoAjuda>
+                            </S.FormGroup>
+                            <S.FormGroup>
+                                <S.ButtonCadastro>
+                                    <S.TextoButton2>Adicionar <br/>Paciente</S.TextoButton2>
+                                    <img src="/plus.svg" alt="Adicionar novo paciente" style={{ width: 24, height: 24 }} />
+                                </S.ButtonCadastro>
+                            </S.FormGroup>
 
-    </form>
-    </S.Container>
-  )
+                        </S.FormRow>
+
+                    </S.FormGroup>
+                    <S.FormGroup>
+                        <S.FormFonte htmlFor="paciente">Profissional:</S.FormFonte>
+                        <S.SelectForm id="profissional" name="profissional">
+                            <option value="profissional1">Profissional 1</option>
+                            <option value="profissional2">Profissional 2</option>
+                            <option value="profissional3">Profissional 3</option>
+                        </S.SelectForm>
+
+                    </S.FormGroup>
+                    <S.FormRow>
+                        <S.FormGroup>
+                            <S.FormFonte htmlFor="data">Data da Visita:</S.FormFonte>
+                            <S.DataForm type="date" id="data" name="data" />
+                        </S.FormGroup>
+                        <S.FormGroup>
+                            <S.FormFonte htmlFor="Turno">Turno:</S.FormFonte>
+                            <S.SelectForm id="turno" name="turno">
+                                <option value="manha">Manhã</option>
+                                <option value="tarde">Tarde</option>
+                            </S.SelectForm>
+                        </S.FormGroup>
+                    </S.FormRow>
+                    <S.FormGroup>
+                        <S.FormFonte htmlFor="observacoes">Observações:</S.FormFonte>
+                        <S.ObservacoesForm id="observacoes" name="observacoes" />
+                    </S.FormGroup>
+                    <S.FormGroup>
+                        <button type="submit">Agendar</button>
+                    </S.FormGroup>
+                </form>
+            </div>
+
+        </S.Container >
+    )
 }
 
 export default Principal
--- sistemaFront/src/pages/agendamento/style.ts
+++ sistemaFront/src/pages/agendamento/style.ts
@@ -4,10 +4,99 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 100%;
 `
 
+export const AgendamentoTitle = styled.h1`
+  font-size: 30px;
+    font-weight: bold;
+    margin-bottom: 20px;
+    `
+
 export const FormGroup = styled.div`
-  margin-bottom: 16px;
-  display: flex;
-  flex-direction: column;
-  `
+    margin-bottom: 16px;
+    display: flex;
+    flex-direction: column;
+    min-width: 18rem;
+    @media (max-width: 600px) {
+        min-width: 100px;
+        max-width: 100%;
+        margin-bottom: 12px;
+    }
+`
+
+export const FormFonte = styled.label`
+    font-size: 20px;
+    margin-bottom: 8px;
+    align-self: flex-start;
+    `
+export const SelectForm = styled.select`
+    padding: 8px;
+    border-radius: 15px;
+    border: 1px solid #444444;
+    font-size: 16px;
+    background-color: #E8E8E8;
+    `
+
+export const DataForm = styled.input`
+    padding: 8px;
+    border-radius: 15px;
+    font-size: 16px;
+    background-color: #E8E8E8;
+    border: 1px solid #444444;
+`
+
+export const ObservacoesForm = styled.textarea`
+    padding: 8px;
+    border-radius: 15px;
+    border: 1px solid #444444;
+    font-size: 16px;
+    background-color: #E8E8E8;
+    resize: vertical;
+    min-height: 60px;
+    `
+
+export const FormRow = styled.div`
+    display: flex;
+    justify-content: start;
+    gap: 5%;
+` 
+
+export const FormAgendamento = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100vw;
+    max-width: 100vw;
+    margin-top: 20px;
+    padding: 20px;
+    box-sizing: border-box;
+`
+export const TextoAjuda = styled.p`
+    font-size: 16px;
+    color:rgb(10, 10, 10);
+    margin-top: 8px;
+`
+
+export const ButtonCadastro = styled.button`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  background-color: #98B8F3;
+  color: black;
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+  font-size: 1.3rem;
+
+  &:hover {
+    background-color:#81a8f0;
+  }
+`
+
+export const TextoButton2 = styled.p`
+  font-size: 1rem;
+  font-weight: 500;
+  color: black;
+  text-align: center;
+`
