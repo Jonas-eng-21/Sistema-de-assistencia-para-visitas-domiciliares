5fe7923241408d8cf671506596962faf9478a69d
Anna Victoria Feliciana <anna.feliciana@upe.br>
2025-05-27 14:14:31 -0300

    Ajustes cadastro de usuario

--- sistemaFront/src/pages/cadastroUser/index.tsx
+++ sistemaFront/src/pages/cadastroUser/index.tsx
@@ -62,6 +62,7 @@ const Cadastro = () => {
         <S.Card>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="padd">
+              <S.TituloForm>Nome:</S.TituloForm>
               <TextField
                 fullWidth
                 label="Nome"
@@ -72,6 +73,7 @@ const Cadastro = () => {
               />
             </div>
             <div className="padd">
+              <S.TituloForm>CPF:</S.TituloForm>
               <TextField
                 fullWidth
                 label="CPF"
@@ -82,6 +84,7 @@ const Cadastro = () => {
               />
             </div>
             <div className="padd">
+              <S.TituloForm>Conselho Regional:</S.TituloForm>
               <TextField
                 fullWidth
                 label="Conselho Regional"
@@ -92,6 +95,7 @@ const Cadastro = () => {
               />
             </div>
             <div className="padd">
+              <S.TituloForm>Email:</S.TituloForm>
               <TextField
                 fullWidth
                 label="Email"
@@ -102,6 +106,7 @@ const Cadastro = () => {
               />
             </div>
             <div className="padd">
+              <S.TituloForm>Senha:</S.TituloForm>
               <FormControl variant="filled" fullWidth>
                 <InputLabel htmlFor="filled-password">Senha</InputLabel>
                 <FilledInput
@@ -129,6 +134,7 @@ const Cadastro = () => {
               </FormControl>
             </div>
             <div className="padd">
+              <S.TituloForm>Profissão:</S.TituloForm>
               <FormControl fullWidth variant="filled">
                 <InputLabel id="profissao-label">Profissão</InputLabel>
                 <Select
@@ -168,6 +174,23 @@ const Cadastro = () => {
               className="button"
               fullWidth
               disabled={!isValid}
+              sx={{
+                backgroundColor: "#98B8F3",
+                color: "#000000",
+                padding: "10px 0",
+                borderRadius: "8px",
+                fontSize: "1rem",
+                boxShadow: "0 2px 8px rgba(25, 118, 210, 0.15)",
+                textTransform: "none",
+                transition: "background 0.2s",
+                "&:hover": {
+                  backgroundColor: "#6f87b3",
+                },
+                "&.Mui-disabled": {
+                  backgroundColor: "#bdbdbd",
+                  color: "#000000",
+                },
+              }}
             >
               Cadastrar
             </Button>
