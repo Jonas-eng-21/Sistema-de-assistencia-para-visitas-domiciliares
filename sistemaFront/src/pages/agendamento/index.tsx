e62f7d979634270448c5d00ba8187650f82787e6
Jonas-eng-21 <jonas.eng.software@gmail.com>
2025-06-04 22:42:58 -0300

    adiciona formulário e integração de criação de agendamento com API

--- sistemaFront/src/pages/agendamento/index.tsx
+++ sistemaFront/src/pages/agendamento/index.tsx
@@ -8,11 +8,50 @@ import type { Patient } from "../../models/Patient";
 import { Button } from "@mui/material";
 import ControlPointTwoToneIcon from "@mui/icons-material/ControlPointTwoTone";
 import { useNavigate } from "react-router-dom";
+import { createScheduleAPI } from "../../services/AgendamentoService";
+import { toast } from "react-toastify";
 
 const Principal = () => {
   const [pacientes, setPacientes] = useState<Patient[]>([]);
   const [profissionais, setProfissionais] = useState<User[]>([]);
   const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    pacienteId: "",
+    profissionalId: "",
+    data: "",
+    turno: "MANHA",
+    motivo: "",
+    prioridade: "ALTA",
+    observacoes: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const agendamento = {
+        pacienteId: parseInt(formData.pacienteId),
+        profissionalId: parseInt(formData.profissionalId),
+        dataVisita: formData.data,
+        turno: formData.turno.toUpperCase(),
+        motivoDoAtendimento: formData.motivo,
+        prioridade:
+          formData.prioridade === "0"
+            ? "ALTA"
+            : formData.prioridade === "1"
+            ? "MEDIA"
+            : "BAIXA",
+        observacao: formData.observacoes || undefined,
+      };
+
+      await createScheduleAPI(agendamento);
+      toast.success("Agendamento criado com sucesso!");
+    } catch (error) {
+      console.error("Erro ao criar agendamento:", error);
+      toast.error("Erro ao criar agendamento.");
+    }
+  };
 
   useEffect(() => {
     document.title = "Página Inicial - Agendamento de visitas";
@@ -45,12 +84,19 @@ const Principal = () => {
         }}
       >
         <S.AgendamentoTitle>Agendamento de Visitas</S.AgendamentoTitle>
-        <form style={{ width: "70%", height: "100%" }}>
+        <form style={{ width: "70%", height: "100%" }} onSubmit={handleSubmit}>
           <S.FormGroup>
             <S.FormRow style={{ alignItems: "end", width: "100%" }}>
               <S.FormGroup style={{ minWidth: "40%", width: "100%" }}>
                 <S.FormFonte htmlFor="paciente">Paciente:</S.FormFonte>
-                <S.SelectForm id="paciente" name="paciente">
+                <S.SelectForm
+                  onChange={(e) =>
+                    setFormData({ ...formData, pacienteId: e.target.value })
+                  }
+                  id="paciente"
+                  name="paciente"
+                  value={formData.pacienteId}
+                >
                   {pacientes.map((paciente) => (
                     <option key={paciente.id} value={paciente.id}>
                       {paciente.nome}
@@ -86,6 +132,10 @@ const Principal = () => {
               id="profissional"
               name="profissional"
               style={{ width: "43%" }}
+              value={formData.profissionalId}
+              onChange={(e) =>
+                setFormData({ ...formData, profissionalId: e.target.value })
+              }
             >
               {profissionais.map((prof) => (
                 <option key={prof.id} value={prof.id}>
@@ -97,19 +147,76 @@ const Principal = () => {
           <S.FormRow>
             <S.FormGroup>
               <S.FormFonte htmlFor="data">Data da Visita:</S.FormFonte>
-              <S.DataForm type="date" id="data" name="data" />
+              <S.DataForm
+                type="date"
+                id="data"
+                name="data"
+                value={formData.data}
+                onChange={(e) =>
+                  setFormData({ ...formData, data: e.target.value })
+                }
+              />
             </S.FormGroup>
             <S.FormGroup>
               <S.FormFonte htmlFor="Turno">Turno:</S.FormFonte>
-              <S.SelectForm id="turno" name="turno">
-                <option value="manha">Manhã</option>
-                <option value="tarde">Tarde</option>
+              <S.SelectForm
+                id="turno"
+                name="turno"
+                value={formData.turno}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    turno: e.target.value.toUpperCase(),
+                  })
+                }
+              >
+                <option value="MANHA">Manhã</option>
+                <option value="TARDE">Tarde</option>
+              </S.SelectForm>
+            </S.FormGroup>
+          </S.FormRow>
+          <S.FormRow>
+            <S.FormGroup style={{ width: "100%" }}>
+              <S.FormFonte htmlFor="motivoAtendimento">
+                Motivo do Atendimento:
+              </S.FormFonte>
+              <S.ObservacoesForm
+                id="motivoAtendimento"
+                name="motivoAtendimento"
+                placeholder="Descreva brevemente o motivo do atendimento"
+                value={formData.motivo}
+                onChange={(e) =>
+                  setFormData({ ...formData, motivo: e.target.value })
+                }
+              />
+            </S.FormGroup>
+
+            <S.FormGroup style={{ width: "100%" }}>
+              <S.FormFonte htmlFor="prioridade">Prioridade:</S.FormFonte>
+              <S.SelectForm
+                id="prioridade"
+                name="prioridade"
+                value={formData.prioridade}
+                onChange={(e) =>
+                  setFormData({ ...formData, prioridade: e.target.value })
+                }
+              >
+                <option value="0">Vermelho</option>
+                <option value="1">Amarelo</option>
+                <option value="2">Verde</option>
               </S.SelectForm>
             </S.FormGroup>
           </S.FormRow>
           <S.FormGroup>
             <S.FormFonte htmlFor="observacoes">Observações:</S.FormFonte>
-            <S.ObservacoesForm id="observacoes" name="observacoes" />
+            <S.ObservacoesForm
+              id="observacoes"
+              name="observacoes"
+              value={formData.observacoes}
+              onChange={(e) =>
+                setFormData({ ...formData, observacoes: e.target.value })
+              }
+            />
           </S.FormGroup>
           <S.FormRow style={{ justifyContent: "center", marginTop: "30px" }}>
             <S.ButtonVoltar type="button">Voltar</S.ButtonVoltar>
