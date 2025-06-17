ecc06437684aad043de77d0eda08003d91df4530
PedroFerreiraLopes <jpedro.serrita@gmail.com>
2025-06-17 16:25:35 -0300

    CORS: Encapsulamento para WebMvcConfigurer corsConfigurer

--- BackEnd/src/main/java/br/com/projeto2/aajjl/config/cors.java
+++ BackEnd/src/main/java/br/com/projeto2/aajjl/config/cors.java
@@ -7,12 +7,17 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 public class cors implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permite todas as rotas
-                .allowedOrigins("https://sistema-de-assistencia-para-visitas.onrender.com/") // Permite o frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todas as rotas
+                        .allowedOrigins("https://sistema-de-assistencia-para-visitas.onrender.com/") // Permite o frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
