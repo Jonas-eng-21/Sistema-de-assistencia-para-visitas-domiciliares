a0f905890a8b00f125431c5dafd1bcb40b92d3ae
Anna Victoria Feliciana <anna.feliciana@upe.br>
2025-06-18 20:41:17 -0300

    Padronização agendamento de visita

--- sistemaFront/src/pages/agendamento/index.tsx
+++ sistemaFront/src/pages/agendamento/index.tsx
@@ -96,7 +96,7 @@ const AgendarVisita = () => {
         motivoDoAtendimento: data.motivoDoAtendimento,
         observacao: data.observacao || "",
       };
-      
+
       await createScheduleAPI(agendamentoParaAPI);
       toast.success("Agendamento criado com sucesso!");
       navigate('/principal');
@@ -113,42 +113,49 @@ const AgendarVisita = () => {
       <S.Card>
         <form onSubmit={handleSubmit(onSubmit)}>
           <S.FormRow>
-            <Controller
-              name="pacienteId"
-              control={control}
-              render={({ field }) => (
-                <FormControl fullWidth variant="filled">
-                  <InputLabel>Paciente</InputLabel>
-                  <Select {...field} error={!!errors.pacienteId}>
-                    {pacientes.map((paciente) => (
-                      <MenuItem key={paciente.id} value={paciente.id}>
-                        {paciente.nome}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              )}
-            />
-            <Controller
-              name="userId"
-              control={control}
-              render={({ field }) => (
-                <FormControl fullWidth variant="filled">
-                  <InputLabel>Profissional</InputLabel>
-                  <Select {...field} error={!!errors.userId}>
-                    {profissionais.map((prof) => (
-                      <MenuItem key={prof.id} value={prof.id}>
-                        {prof.nome} - {prof.profissao}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              )}
-            />
+            <div style={{ width: "100%", paddingRight: 8 }}>
+              <S.TituloForm>Paciente:</S.TituloForm>
+              <Controller
+                name="pacienteId"
+                control={control}
+                render={({ field }) => (
+                  <FormControl fullWidth variant="filled">
+                    <Select {...field} error={!!errors.pacienteId}>
+                      {pacientes.map((paciente) => (
+                        <MenuItem key={paciente.id} value={paciente.id}>
+                          {paciente.nome}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                )}
+              />
+            </div>
+            <div style={{ width: "100%", paddingLeft: 8 }}>
+              <S.TituloForm>Profissional:</S.TituloForm>
+              <Controller
+                name="userId"
+                control={control}
+                render={({ field }) => (
+                  <FormControl fullWidth variant="filled">
+                    <Select {...field} error={!!errors.userId}>
+                      {profissionais.map((prof) => (
+                        <MenuItem key={prof.id} value={prof.id}>
+                          {prof.nome} - {prof.profissao}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                )}
+              />
+            </div>
           </S.FormRow>
+
 
           {/* Linha 2: Data e Turno */}
           <S.FormRow>
+            <div style={{ width: "100%", paddingRight: 8 }}>
+              <S.TituloForm>Data de agendamento:</S.TituloForm>
             <Controller
               name="dataAgendamento"
               control={control}
@@ -165,6 +172,9 @@ const AgendarVisita = () => {
                 />
               )}
             />
+            </div>
+            <div style={{ width: "100%", paddingLeft: 8 }}>
+              <S.TituloForm>Turno:</S.TituloForm>
             <Controller
               name="turno"
               control={control}
@@ -179,10 +189,13 @@ const AgendarVisita = () => {
                 </FormControl>
               )}
             />
+            </div>
           </S.FormRow>
 
           {/* Linha 3: Motivo e Prioridade */}
           <S.FormRow>
+            <div style={{ width: "100%", paddingRight: 8 }}>
+              <S.TituloForm>Motivo de atendimento:</S.TituloForm>
             <Controller
               name="motivoDoAtendimento"
               control={control}
@@ -199,6 +212,9 @@ const AgendarVisita = () => {
                 />
               )}
             />
+            </div>
+            <div style={{ width: "100%", paddingLeft: 8 }}>
+              <S.TituloForm>Prioridade:</S.TituloForm>
             <Controller
               name="prioridade"
               control={control}
@@ -217,9 +233,12 @@ const AgendarVisita = () => {
                 </FormControl>
               )}
             />
+            </div>
           </S.FormRow>
 
           {/* Linha 4: Observações */}
+          <div style={{ width: "100%", paddingRight: 8 }}>
+              <S.TituloForm>Observações (opcional):</S.TituloForm>
           <Controller
             name="observacao"
             control={control}
@@ -235,27 +254,58 @@ const AgendarVisita = () => {
               />
             )}
           />
+          </div>
 
           {/* Linha 5: Botões */}
           <S.ButtonContainer>
             <Button
-              variant="outlined"
-              color="secondary"
+              variant="contained"
               fullWidth
               onClick={() => navigate(-1)}
+              sx={{
+                backgroundColor: "#bdbdbd",
+                color: "#000000",
+                padding: "10px 0",
+                borderRadius: "8px",
+                fontSize: "1rem",
+                boxShadow: "0 2px 8px rgba(189, 189, 189, 0.15)",
+                textTransform: "none",
+                transition: "background 0.2s",
+                "&:hover": {
+                  backgroundColor: "#9e9e9e",
+                },
+              }}
             >
               Voltar
             </Button>
+
             <Button
               type="submit"
               variant="contained"
-              color="primary"
               fullWidth
               disabled={!isValid}
+              sx={{
+                backgroundColor: "#98B8F3",
+                color: "#000000",
+                padding: "10px 0",
+                borderRadius: "8px",
+                fontSize: "1rem",
+                boxShadow: "0 2px 8px rgba(25, 118, 210, 0.15)",
+                textTransform: "none",
+                transition: "background 0.2s",
+                "&:hover": {
+                  backgroundColor: "#6f87b3",
+                },
+                "&.Mui-disabled": {
+                  backgroundColor: "#bdbdbd",
+                  color: "#000000",
+                },
+              }}
             >
               Salvar Agendamento
             </Button>
           </S.ButtonContainer>
+
         </form>
       </S.Card>
     </S.Container>
